6df325486fda18f12a43624281f913fbe29041d0
Sain <turtle27799@gmail.com>
2025-07-12 15:37:09 +0200

    matching app added + included in urls

---
+++ matching/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.urls import include
+
+app_name = 'matching'
+
+urlpatterns = [
+    # path('match/', views.match_view, name='match'),
+    # path('results/', views.results_view, name='results'),
+]
+
